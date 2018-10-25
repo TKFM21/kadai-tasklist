c31fb234b08dc932c1b6af17e5b4831c7f8f8f08
fumiaki.takizawa <tkzwfm21@gmail.com>
2018-10-25 13:01:04 +0000

    bootstrap grid

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,10 +3,11 @@
 @section('content')
 
 <!-- ここにページ毎のコンテンツを書く -->
-    <h1>NEW Task Create Page</h1>
+    
 
     <div class="row">
-        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            <h1>NEW Task Create Page</h1>
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
                 <div class="form-group">
